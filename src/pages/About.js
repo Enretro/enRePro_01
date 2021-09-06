0b7dc41b8d459a396f26dc881e0d7258e77725e5
Enretro <envpogled@gmail.com>
2021-09-06 16:47:23 +0200

    01 enRePro

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about clean-system</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione, similique rem! Repudiandae inventore quae mollitia autem ea amet ipsam officiis eaque eum nemo. Optio nostrum incidunt voluptas recusandae! Id aperiam pariatur omnis quibusdam dolorem ab in corporis eum quis numquam!</p>
+    </section>
+ )
+}
+
+export default About
